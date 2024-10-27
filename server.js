5240b03e813d79cbaca77361cd759b79372d79c0
Aditya <aditya1115.ar@gmail.com>
2024-10-27 22:47:29 +0530

    added chat service

--- server.js
+++ server.js
@@ -3,19 +3,34 @@ const app = require('./app');
 const connectDB = require('./db/mongoConnection');
 const { Server } = require('socket.io');
 const setupWhiteboardSockets = require('./sockets/whiteboardSocket');
+const corsConfig = require('./utils/corsConfig');
 
 
 connectDB();
+
 // Create HTTP server
 const server = http.createServer(app);
+
+// Create socket server
 const io = new Server(server, {
     cors: {
-        origin: "localhost:3000",
+        origin: 'http://localhost:3000',
         methods: ["GET", "POST"]
     }
-})
+});
 
-setupWhiteboardSockets(io);
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    
+    console.log("Socket ", socket.id)
+    socket.on('send_message', (message) => {
+        io.emit('receive_message', message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
